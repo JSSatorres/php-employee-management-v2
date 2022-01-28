e410d0dcbb35109aca8e288114a1a3877238ae8d
RB <87038824+MrMhirs@users.noreply.github.com>
2022-01-28 14:19:22 +0100

    Pull Login Controller

--- src/library/sessionHelper.php
+++ src/library/sessionHelper.php
@@ -6,13 +6,13 @@ require_once('loginManager.php');
 //check sesion & check sesion time
 function checkSessionTime()
 {
-    session_start();
-if (isset($_SESSION["logintime"])){
+
+    if (isset($_SESSION["logintime"])) {
         if (time() >= $_SESSION["logintime"] + 600) {
             destroySession();
             header('Location: ../index.php?logout_time_expired');
         }
-}
+    }
 }
 
 function checkSession()
